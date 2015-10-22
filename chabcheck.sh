c5b8da7aed25afe071ee2b6dccefc9f98fbf147c
chabbath <codered9001@gmail.com>
2015-10-22 17:23:02 +0200

    version v0.0.2

--- chabcheck.sh
+++ chabcheck.sh
@@ -2,17 +2,20 @@
 function f {
 for i in $1/*; do
 	if [ -d $i ]; then
-	echo -e "[\e[33mDIR\e[0m] "$i; f $i; 
+	echo -e "[\e[33mDIR\e[0m] " $i; f $i; 
 	else
 		if [ -f $i ]; then         
 			echo -e "[\e[32mFILE\e[0m] " "\e[34m" `file -b $i` "\e[0m" `md5sum $i`;
+		else
+			echo -e "\e[31m[EE]\e[0m " $i;
+			break;		
 		fi
 	fi
 	
 done;
 }
 if [ $1 == "-v" ]; then
-	echo "chabcheck v0.0.1a";
+	echo "chabcheck v0.0.2";
 else
  if [ $1 == "-h" ]; then
 	echo "Frist paramater is a destination";
